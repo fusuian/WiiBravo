863dc4ef60a9eb3afa1ef8ea18cfb28fa7196f96
ASAHI,Michiharu <fusuian@gmail.com>
2018-06-11 01:20:33 +0900

    サンプルのコメント、フォーマットを変更

--- PS2WiiBravo.ino
+++ PS2WiiBravo.ino
@@ -37,38 +37,18 @@ void setup(){
   Serial.begin(57600);
   
   delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
-   
-  //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
   
-  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
   error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
   
-  if(error == 0){
+  if (error == 0){
     Serial.print("Found Controller, configured successful ");
-    Serial.print("pressures = ");
-	if (pressures)
-	  Serial.println("true ");
-	else
-	  Serial.println("false");
-	Serial.print("rumble = ");
-	if (rumble)
-	  Serial.println("true)");
-	else
-	  Serial.println("false");
-    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
-    Serial.println("holding L1 or R1 will print out the analog stick values.");
-    Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
-  }  
-  else if(error == 1)
+  } else if (error == 1) {
     Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
-   
-  else if(error == 2)
+  } else if (error == 2) {
     Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
-
-  else if(error == 3)
+  } else if (error == 3) {
     Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
-  
-//  Serial.print(ps2x.Analog(1), HEX);
+  }
   
   type = ps2x.readType(); 
   switch(type) {
@@ -90,37 +70,11 @@ void setup(){
 byte vtriangle, vcircle, vcross, vsquare;
 
 void loop() {
-  /* You must Read Gamepad to get new values and set vibration values
-     ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
-     if you don't enable the rumble, use ps2x.read_gamepad(); with no values
-     You should call this at least once a second
-   */  
-  if(error == 1) //skip loop if no controller found
+  if (error == 1) {
     return; 
-  
-  ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
-  
-  if(ps2x.Button(PSB_START))         //will be TRUE as long as button is pressed
-    Serial.println("Start is being held");
-  if(ps2x.Button(PSB_SELECT))
-    Serial.println("Select is being held");      
-
-  if(ps2x.Button(PSB_PAD_UP)) {      //will be TRUE as long as button is pressed
-    Serial.print("Up held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
   }
-  if(ps2x.Button(PSB_PAD_RIGHT)){
-    Serial.print("Right held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_LEFT)){
-    Serial.print("LEFT held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_DOWN)){
-    Serial.print("DOWN held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-  }   
+    
+  ps2x.read_gamepad(false, vibrate);
 
   vibrate = ps2x.Analog(PSAB_CROSS);  //this will set the large motor vibrate speed based on how hard you press the blue (X) button
   if (ps2x.NewButtonState()) {        //will be TRUE if any button changes state (on to off, or off to on)
@@ -150,15 +104,5 @@ void loop() {
   }
 
 
-  if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) { //print stick values if either is TRUE
-    Serial.print("Stick Values:");
-    Serial.print(ps2x.Analog(PSS_LY), DEC); //Left stick, Y axis. Other options: LX, RY, RX  
-    Serial.print(",");
-    Serial.print(ps2x.Analog(PSS_LX), DEC); 
-    Serial.print(",");
-    Serial.print(ps2x.Analog(PSS_RY), DEC); 
-    Serial.print(",");
-    Serial.println(ps2x.Analog(PSS_RX), DEC); 
-  }
   delay(50);  
 }
