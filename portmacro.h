bd9fde8709d22c914845cdb05df96d0f30b7ac85
ASAHI,Michiharu <fusuian@gmail.com>
2018-10-01 20:32:37 +0900

    isPinを更新

--- portmacro.h
+++ portmacro.h
@@ -16,16 +16,26 @@
  * along with portmacro.h.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef __PORTMACRO_H__
+#define __PORTMACRO_H__
+
 #define PORT(n) ( (n <=  7)? PORTD : \
                 ( (n <= 13)? PORTB : \
                 ( (n <= 19)? PORTC : PORTB)))
+
+#define PIN(n) ( (n <=  7)? PIND : \
+               ( (n <= 13)? PINB : \
+               ( (n <= 19)? PINC : PINB)))
 
 #define REG(n)  ( (n <=  7)? (n) : \
                 ( (n <= 13)? (n - 8) : \
                 ( (n <= 19)? (n - 14) : (13 - 8) )))
 
+// set pin HIGH
 #define portOn(p)  ( PORT(p) |=  _BV(REG(p)) )
+// set pin LOW
 #define portOff(p) ( PORT(p) &= ~_BV(REG(p)) )
+// check pin state
+#define isPin(p)  ( (PIN(p) & _BV(REG(p))) == 0 )
 
-#define isPin(p)  ( (PORT(p) & ~_BV(REG(p))) != 0 )
-
+#endif // __PORTMACRO_H__
